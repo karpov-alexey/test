df48fb63a1e06b20a8cdef9186bba8d732cedb24
Alexey Karpov <you@example.com>
2017-03-30 14:50:54 +0300

    Added obfuscation

--- hello_world/src/main/java/Hello.java
+++ hello_world/src/main/java/Hello.java
@@ -1,10 +1,46 @@
 /**
  * Created by Alexey on 29.03.2017.
  */
+
+class Test
+{
+    Test()
+    {
+        System.out.println("Hello world");
+        String s = "hii";
+        switch(s) {
+            case "hid":
+                System.out.println(s);
+                break;
+
+            default:
+                System.out.println(s);
+                break;
+        }
+    }
+}
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        String s = "hi1";
+        String s = "hi";
+        switch(s) {
+            case "hi":
+                System.out.println(s);
+                break;
+
+            default:
+                System.out.println(s);
+                break;
+        }
+
+        new Hello().ss();
+        new Test();
+    }
+
+    private void ss()
+    {
+        System.out.println("Hello world");
+        String s = "hi";
         switch(s) {
             case "hi":
                 System.out.println(s);
